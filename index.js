97cd20b7c3acbe52ec2860ee5cb4b0049541141a
Eugene Moti <78027361+Eugene-dev-opps@users.noreply.github.com>
2023-04-30 23:11:58 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,57 +1,89 @@
-//star
-const stars = document.querySelectorAll('.star');
-stars.forEach(star => {
-   star.addEventListener('click', toggleStar);
-});
+const fs = require('fs').promises;
+const path = require('path');
+const process = require('process');
+const {authenticate} = require('@google-cloud/local-auth');
+const {google} = require('googleapis');
 
-function toggleStar() {
-   this.classList.toggle('selected');
-}
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
 
-function selectAllEmailRows() {
-  const selectAllCheckbox = document.getElementById('select-all-checkbox');
-  const emailCheckboxes = document.querySelectorAll('.emailRow__checkbox input[type="checkbox"]');
-  
-  for (let i = 0; i < emailCheckboxes.length; i++) {
-    emailCheckboxes[i].checked = selectAllCheckbox.checked;
+/**
+ * Reads previously authorized credentials from the save file.
+ *
+ * @return {Promise<OAuth2Client|null>}
+ */
+async function loadSavedCredentialsIfExist() {
+  try {
+    const content = await fs.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials);
+  } catch (err) {
+    return null;
   }
 }
 
+/**
+ * Serializes credentials to a file compatible with GoogleAUth.fromJSON.
+ *
+ * @param {OAuth2Client} client
+ * @return {Promise<void>}
+ */
+async function saveCredentials(client) {
+  const content = await fs.readFile(CREDENTIALS_PATH);
+  const keys = JSON.parse(content);
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: 'authorized_user',
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
+  });
+  await fs.writeFile(TOKEN_PATH, payload);
+}
 
-//next page navivigation
-function navigateToPage() {
-    const emailRows = document.querySelectorAll('.emailRow:not(.exclude)');
-    let emailRowHTML = '';
-    emailRows.forEach(row => emailRowHTML += row.outerHTML);
-    const nextPageURL = `Mail.html?emailRow=${encodeURIComponent(emailRowHTML)}`;
-    window.location.href = nextPageURL;
-
-    // Get the email row element that was clicked
-  var emailRow = event.target.closest('.emailRow');
-
-  // Add the "emailRow--clicked" class to the clicked email row
-  emailRow.classList.add('emailRow--clicked');
+/**
+ * Load or request or authorization to call APIs.
+ *
+ */
+async function authorize() {
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
+    return client;
   }
-
- function toggleReadState(event) {
-  const emailRow = event.target.closest('.emailRow');
-  emailRow.classList.toggle('unread');
-  const markIcon = emailRow.querySelector('.mark_chat_unread');
-  markIcon.innerHTML = emailRow.classList.contains('unread') ? 'mark_chat_read' : 'mark_chat_unread';
-  const emailSubject = emailRow.querySelector('.emailRow__subject');
-  emailSubject.style.fontWeight = emailRow.classList.contains('unread') ? 'normal' : 'bold';
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (client.credentials) {
+    await saveCredentials(client);
+  }
+  return client;
 }
 
-
-function toggleReadState(event) {
-  const emailRow = event.target.closest('.emailRow');
-  emailRow.classList.toggle('unread');
-  const markIcon = emailRow.querySelector('.mark_chat_unread');
-  markIcon.innerHTML = emailRow.classList.contains('unread') ? 'mark_chat_read' : 'mark_chat_unread';
-  const emailSubject = emailRow.querySelector('.emailRow__subject');
-  emailSubject.style.fontWeight = emailRow.classList.contains('unread') ? 'normal' : 'bold';
+/**
+ * Lists the labels in the user's account.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+async function listLabels(auth) {
+  const gmail = google.gmail({version: 'v1', auth});
+  const res = await gmail.users.labels.list({
+    userId: 'me',
+  });
+  const labels = res.data.labels;
+  if (!labels || labels.length === 0) {
+    console.log('No labels found.');
+    return;
+  }
+  console.log('Labels:');
+  labels.forEach((label) => {
+    console.log(`- ${label.name}`);
+  });
 }
 
-
-
-
+authorize().then(listLabels).catch(console.error);
